941983a68fad71c749a7597faf7a5a32a1b9f479
Jack Li <lihz01051@gmail.com>
2018-11-24 12:09:05 -0800

    fixed easter egg left behind for next team proj

--- database/index.js
+++ database/index.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 // use own mongoUri
-const mongoUri = '';
+// const mongoUri = '';
+const mongoUri = 'mongodb+srv://jacli1314:passwordeeznuts1@cluster0-wrvnw.mongodb.net/galleries?retryWrites=true';	
 const db = mongoose.connect(mongoUri, { useNewUrlParser: true });
 module.exports = db;
